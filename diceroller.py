7f71f161f9b9489261751be8ad2b0dac84dd1e38
jijinkh <jijinkharidas10@gmail.com>
2019-08-18 23:07:43 +0530

    made it a function

--- diceroller.py
+++ diceroller.py
@@ -1,14 +1,9 @@
-"""
-The program should contain a random function which randomizes from 1-6 and shows the input as dice
-1= *
-2 = **
-3= ***
-"""
 import random
-c=(random.randint(1,6))
-print(c)
-a={1:"*",2:"*   *",3:"*   *\n  *",4:"*   *\n*   *",5:"*   *\n  *\n*   *",6:"*   *\n*   *\n*   *"}
-print(a[c])
+def dice():
+    c=(random.randint(1,6))
+    print(c)
+    a={1:"*", 2:"* *", 3:"* *\n *", 4:"* *\n* *", 5:"* *\n*\n* *", 6:"* *\n* *\n* *"}
+    print(a[c])
 
 
 
