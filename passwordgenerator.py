e2cb17253dc0c1d6070727b9efdc37fc26ea21a9
jijinkh <jijinkharidas10@gmail.com>
2019-08-19 19:14:53 +0530

    few changes made

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -3,42 +3,37 @@ Generate a password of 10 characters with letters symbols and numbers
 choose an option to opt out or include caps, symbols, numbers
 """
 import random
-print("Date a SouthIndian so that it can be a strong password!!!")
-usr_inpC = raw_input("Generate with caps? : y/n")
-usr_inpSymb = raw_input("Generate pwd with sumbols? : y/n")
-user_inpNum = raw_input("Generate pwd with numbers? : y/n")
+import string
 
-print (usr_inpC+"\n"+usr_inpSymb+"\n"+user_inpNum)
-
-def allFun():	
-	chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*?'
+def allFun():
+	chars = string.ascii_uppercase + string.ascii_lowercase + '!@#$%^&*?'
 	password = ''
 	for c in range(10):
 		password += random.choice(chars)
 	print(password)
 
-def Num():	
-	chars = 'abcdefghijklmnopqrstuvwxyz1234567890'
+def Num():
+	chars = string.ascii_lowercase + '1234567890'
 	password = ''
 	for c in range(10):
 		password += random.choice(chars)
 	print(password)
 
-def sym():	
-	chars = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*?'
+def sym():
+	chars = string.ascii_lowercase + '!@#$%^&*?'
 	password = ''
 	for c in range(10):
 		password += random.choice(chars)
 	print(password)
 
-def letters():	
-	chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def letters():
+	chars = string.ascii_lowercase + string.ascii_uppercase
 	password = ''
 	for c in range(10):
 		password += random.choice(chars)
 	print(password)
 
-def numSymb():	
+def numSymb():
 	chars = '1234567890!@#$%^&*?'
 	password = ''
 	for c in range(10):
@@ -46,32 +41,32 @@ def numSymb():
 	print(password)
 
 
-def capsSymb():	
-	chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*?'
+def capsSymb():
+	chars = string.ascii_uppercase + '!@#$%^&*?'
 	password = ''
 	for c in range(10):
 		password +=random.choice(chars)
 	print(password)
 
-def capsNum():	
-	chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+def capsNum():
+	chars = string.ascii_uppercase + '1234567890'
 	password = ''
 	for c in range(10):
 		password+=random.choice(chars)
 	print(password)
 
+def generate():
+	usr_inpC = input("Generate with caps? (y/n): ")
+	usr_inpSymb = input("Generate pwd with sumbols? (y/n): ")
+	user_inpNum = input("Generate pwd with numbers? (y/n): ")	
 
-if (usr_inpC == "y" and usr_inpSymb == "y" and user_inpNum == "y"):
-	allFun()
-elif (usr_inpC == "y" and usr_inpSymb == "y" and user_inpNum == "n"):
-	capsSymb()
-elif (usr_inpC == "y" and user_inpNum == "y" and usr_inpSymb == "n"):
-	capsNum()
-elif (usr_inpC == "n" and usr_inpSymb == "y" and user_inpNum == "y"):
-  numSymb()
-else:
-   print("please enter the combination of any two option")
-	
-
-
-
+	if (usr_inpC == "y" and usr_inpSymb == "y" and user_inpNum == "y"):
+		allFun()
+	elif (usr_inpC == "y" and usr_inpSymb == "y" and user_inpNum == "n"):
+		capsSymb()
+	elif (usr_inpC == "y" and user_inpNum == "y" and usr_inpSymb == "n"):
+		capsNum()
+	elif (usr_inpC == "n" and usr_inpSymb == "y" and user_inpNum == "y"):
+	  numSymb()
+	else:
+	   print("please enter the combination of any two option")
