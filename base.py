5df504bd5fda397cd6e3ea06898b057ff61824cd
jijinkh <jijinkharidas10@gmail.com>
2019-08-23 20:40:23 +0530

    changes made

--- base.py
+++ base.py
@@ -4,29 +4,32 @@ from diceroller import dice
 from guess import guesser
 import hangman
 import currencyconvertor
-from passwordgenerator import *
+from passwordgenerator import generate
 import tictactoe
 import minesweeper
 
 while True:
     os.system("clear")
     try:
-        choice = int(input(("""Enter your choice:\n1.Dice Roller\n2.Guess the number game\n3.Hangman Game\n4.Currency convertor\n5.Random Password Generator\n6.Tic Tac Toe\n7.Minesweeper\n0.Exit\n""")))
-    except:
+        choice = int(input(("""
+Enter your choice:\n1.Dice Roller
+2.Guess the number game\n3.Hangman Game\n4.Currency currency 
+5.Random Password Generator\n6.Tic Tac Toe\n7.Minesweeper\n0.Exit\n""")))
+    except ValueError:
         print("Wrong input")
         time.sleep(0.2)
         continue
     os.system("clear")
     if choice == 1:
-    	dice()
-    	break
+        dice()
+        break
     elif choice == 2:
         guesser()
         break
     elif choice == 3:
         hangman()
     elif choice == 4:
-        inrconvertor()
+        currencyconvertor()
     elif choice == 5:
         generate()
         break
